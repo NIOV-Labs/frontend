15f9aabcb544c1df2d2680df76d15c557a85a7e1
Jesse Robinson <j3robinson1@gmail.com>
2024-05-31 23:43:47 -0400

    fixed dynamic chainId linkage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { ethers } from "ethers"
-import Dashboard from './Dashboard'
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ethers } from 'ethers';
+import Dashboard from './Dashboard';
 import Home from './Home';
 import UserNavBar from '../components/UserNavBar';
 import ABTsProject from './ABTsProject';
@@ -11,64 +11,64 @@ import Settings from './Settings';
 import Developer from './Developer';
 import Support from './Support';
 import ABTDetails from './ABTDetails';
-import chainConfig from '../../utils/ChainConfig.json'
-import ABTAbi from '../../utils/interfaces/AssetBoundToken.json'
-import MarketAbi from '../../utils/interfaces/NiovMarket.json'
-import MarketReaderAbi from '../../utils/interfaces/MarketReader.json'
+import chainConfig from '../../utils/ChainConfig.json';
+import ABTAbi from '../../utils/interfaces/AssetBoundToken.json';
+import MarketAbi from '../../utils/interfaces/NiovMarket.json';
+import MarketReaderAbi from '../../utils/interfaces/MarketReader.json';
 import { formatUnits } from 'ethers';
-import fs from 'fs';
 
 function App() {
   const [client, setClient] = useState({
-      account: null,
-      signer: null,
-      chainId: null,
-      provider: null
+    account: null,
+    signer: null,
+    chainId: null,
+    provider: null
   });
   const [hasWeb3, setHasWeb3] = useState(false);
-  const [abt, setAbt] = useState({})
-  const [market, setMarket] = useState([])
-  const [reader, setReader] = useState({})
+  const [abt, setAbt] = useState({});
+  const [market, setMarket] = useState([]);
+  const [reader, setReader] = useState({});
 
   const web3Handler = async () => {
-      var account; var chainId;
+    var account;
+    var chainId;
 
-      await window.ethereum.request({ method: 'eth_requestAccounts' })
-      .then((accounts) => {
-        account = accounts[0] });
+    await window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
+      account = accounts[0];
+    });
 
-      await window.ethereum.request({ method: 'eth_chainId' })
-      .then((res) => {
-        chainId = res });
+    await window.ethereum.request({ method: 'eth_chainId' }).then((res) => {
+      chainId = parseInt(res, 16);
+    });
 
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const balance = await provider.getBalance(account);
-      let balanceInEther = formatUnits(balance, 'ether');
-      balanceInEther = Math.floor(balanceInEther)
-      
-      const signer = await provider.getSigner();
-      const nativeCurrency = getNativeCurrency(parseInt(chainId, 16));
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const balance = await provider.getBalance(account);
+    let balanceInEther = formatUnits(balance, 'ether');
+    balanceInEther = Math.floor(balanceInEther);
 
-      setClient({
-        account: account,
-        signer: signer,
-        chainId: parseInt(chainId, 16),
-        provider: provider,
-        balanceInEther,
-        nativeCurrency
-      })
-      loadContracts(signer. chainId)
+    const signer = await provider.getSigner();
+    const nativeCurrency = getNativeCurrency(parseInt(chainId, 16));
+
+    setClient({
+      account: account,
+      signer: signer,
+      chainId: parseInt(chainId, 16),
+      provider: provider,
+      balanceInEther,
+      nativeCurrency
+    });
+    loadContracts(signer, chainId);
   };
 
   const loadContracts = async (signer, chainId) => {
-    const Addresses = JSON.parse(fs.readFileSync(`../../utils/deploymentMap/${chainId}.json`));
+    const Addresses = await import(`../../utils/deploymentMap/${chainId}.json`);
     const abtContract = new ethers.Contract(Addresses.AssetBoundToken, ABTAbi.abi, signer);
     const marketContract = new ethers.Contract(Addresses.NiovMarket, MarketAbi.abi, signer);
-    const readerContract = new ethers.Contract(Addresses.MarketReader, MarketReaderAbi.abi, signer)
-    setReader(readerContract)
-    setAbt(abtContract)
-    setMarket(marketContract)
-  }
+    const readerContract = new ethers.Contract(Addresses.MarketReader, MarketReaderAbi.abi, signer);
+    setReader(readerContract);
+    setAbt(abtContract);
+    setMarket(marketContract);
+  };
 
   const getNativeCurrency = (chainId) => {
     for (const key in chainConfig) {
@@ -76,50 +76,52 @@ function App() {
         return chainConfig[key].nativeCurrency;
       }
     }
-    return null; 
+    return null;
   };
-  
 
   if (window.ethereum) {
-    window.ethereum.on('chainChanged', () => {window.location.reload()});
-    window.ethereum.on('accountsChanged', () => {window.location.reload()});
-    if(!hasWeb3) { setHasWeb3(true); }
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    });
+    window.ethereum.on('accountsChanged', () => {
+      window.location.reload();
+    });
+    if (!hasWeb3) {
+      setHasWeb3(true);
+    }
   }
-
 
   return (
     <BrowserRouter>
-      {/* <div className='flex flex-column justify-center items-start min-h-[100vh] min-w-screen mt-[4.5rem] lg:mt-[6rem] lg:ml-60 xl:ml-72 2xl:ml-96 relative'> */}
       <div className='flex justify-start items-start min-h-[100vh] min-w-screen relative w-full'>
-        {
-          client.account ? 
+        {client.account ? (
           <>
             <UserNavBar client={client} />
             <main className='mt-[4.5rem] lg:mt-[5rem] xl:ml-72 2xl:ml-80 relative w-full'>
               <Routes>
-                <Route path="/dashboard"  element={<Dashboard client={client} market={market} abt={abt} reader={reader}/>}/>
-                <Route path="/abts"  element={<ABTsProject client={client} market={market} abt={abt} reader={reader}/>}/>
-                <Route path="/marketplace"  element={<Marketplace client={client} market={market} abt={abt} reader={reader} />}/>
-                <Route path="/myWallet"  element={<MyWallet client={client}/>}/>
-                <Route path="/settings"  element={<Settings client={client}/>}/>
-                <Route path="/developer"  element={<Developer client={client}/>}/>
-                <Route path="/support"  element={<Support client={client}/>}/>
-                <Route path="/abt/:id"  element={<ABTDetails client={client} market={market} abt={abt} reader={reader} />}/>
+                <Route path="/dashboard" element={<Dashboard client={client} market={market} abt={abt} reader={reader} />} />
+                <Route path="/abts" element={<ABTsProject client={client} market={market} abt={abt} reader={reader} />} />
+                <Route path="/marketplace" element={<Marketplace client={client} market={market} abt={abt} reader={reader} />} />
+                <Route path="/myWallet" element={<MyWallet client={client} />} />
+                <Route path="/settings" element={<Settings client={client} />} />
+                <Route path="/developer" element={<Developer client={client} />} />
+                <Route path="/support" element={<Support client={client} />} />
+                <Route path="/abt/:id" element={<ABTDetails client={client} market={market} abt={abt} reader={reader} />} />
                 <Route path="*" element={<Navigate replace to="/dashboard" />} />
               </Routes>
             </main>
           </>
-          :
+        ) : (
           <>
-           <Routes>
-              <Route path="/" element={<Home client={client} hasWeb3={hasWeb3} web3Handler={web3Handler}/>} />
+            <Routes>
+              <Route path="/" element={<Home client={client} hasWeb3={hasWeb3} web3Handler={web3Handler} />} />
               <Route path="*" element={<Navigate replace to="/" />} />
             </Routes>
           </>
-        }
+        )}
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
