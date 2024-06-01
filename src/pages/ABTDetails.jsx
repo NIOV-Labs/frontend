35204fa00bf3d2ffad567e336c5cc2d785483def
mar69287 <marcoa.ruiz97@gmail.com>
2024-06-01 13:55:21 -0700

    add update and delete listing

--- src/pages/ABTDetails.jsx
+++ src/pages/ABTDetails.jsx
@@ -18,6 +18,10 @@ const ABTDetails = ({ client, market, abt }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadingFunction, setLoadingFunction] = useState(false)
   const [price, setPrice] = useState('0.00');
+  const [showUpdateForm, setShowUpdateForm] = useState(false)
+  const [updatedPrice, setUpdatedPrice] = useState(price)
+  const [deleteListing, setDeleteListing] = useState(false)
+  const [loadingDelete, setLoadingDelete] = useState(false)
 
   const fetchABTDetails = async () => {
     try {
@@ -63,6 +67,7 @@ const ABTDetails = ({ client, market, abt }) => {
             rawValueGas: listing[2]
         }
         console.log(data)
+        setUpdatedPrice(priceUsd)
         setAbtInfo(data)
         setIsLoading(false)
     } catch(err) {
@@ -128,6 +133,13 @@ const ABTDetails = ({ client, market, abt }) => {
     }
   };
 
+  const handleUpdatePriceInput = (e) => {
+    const value = e.target.value;
+    if (/^\d*(\.\d{0,2})?$/.test(value)) {
+      setUpdatedPrice(value);
+    }
+  };
+
   const handleBuyingAbt =  async (e) => {
     e.preventDefault();
     setLoadingFunction(true);
@@ -142,6 +154,34 @@ const ABTDetails = ({ client, market, abt }) => {
       setLoadingFunction(false);
     }
   }
+
+  const handleUpdateListing =  async (e) => {
+    e.preventDefault();
+    if (updatedPrice === abtInfo.priceUsd) return;
+    const pennyAmount = parseInt(parseFloat(updatedPrice) * 100)
+    try {
+      setLoadingFunction(true);
+      await market.updateListing(abt.target, id, pennyAmount);
+      await fetchABTDetails();
+      setLoadingFunction(false); 
+      setShowUpdateForm(false)
+    } catch (err) {
+      console.error('Error purchasing:', err);
+    }
+  }
+
+  const handleDeleteListing = async () => {
+    try {
+      setLoadingDelete(true);
+      await market.destroyListing(abt.target, id);
+      await fetchABTDetails();
+      setDeleteListing(false);
+    } catch (err) {
+      console.error('Error deleting listing:', err);
+    } finally {
+      setLoadingDelete(false);
+    }
+  };
 
   if (isLoading) {
     return (
@@ -162,21 +202,83 @@ const ABTDetails = ({ client, market, abt }) => {
             <div className="w-full pb-2 lg:col-span-2">
                 <h1 className="text-2xl xl:text-3xl font-semibold">{abtInfo.name}</h1>
             </div>
-            <ImageCarousel imgs={abtInfo.images} />
+            <ImageCarousel imgs={abtInfo.images.length > 1 ? abtInfo.images : [abtInfo.document1image]} />
             <div>
                 <Accordian owner={abtInfo.owner.slice(0, 5) + '...' + abtInfo.owner.slice(38, 42)} description={abtInfo.description} details={abtInfo.details} link={abtInfo.document1Link} />
                 {abtInfo.isOwner ? (
                     abtInfo.signerIsSeller ? (
-                        <div className='w-full flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-4'>
-                            <div className='flex flex-col gap-1 justify-center items-center p-4 lg:p-6 shadow-lg w-max rounded-lg'>
-                                <p className='text-gray-700 text-md lg:text-lg font-bold'>{abtInfo.priceGas}</p>
-                                <p className='text-gray-700 text-sm xl:text-md font-medium leading-3'>Gas</p>
+                        <>
+                            <div className='w-full flex flex-col gap-4 justify-center items-center'>
+                                <div className='w-full flex justify-center items-center gap-2 sm:flex-row sm:gap-4'>
+                                    <div className='flex flex-col gap-1 justify-center items-center p-4 lg:p-6 shadow-lg w-max rounded-lg'>
+                                        <p className='text-gray-700 text-md lg:text-lg font-bold'>{abtInfo.priceGas}</p>
+                                        <p className='text-gray-700 text-sm xl:text-md font-medium leading-3'>Gas</p>
+                                    </div>
+                                    <div className='flex flex-col gap-1 justify-center items-center p-4 lg:p-6 shadow-lg w-max rounded-lg'>
+                                        <p className='text-gray-700 text-md lg:text-lg font-bold'>$ {abtInfo.priceUsd}</p>
+                                        <p className='text-gray-700 text-sm xl:text-md font-medium leading-3'>Price</p>
+                                    </div>
+                                </div>
+                                {showUpdateForm ? (
+                                    <div className="w-full max-w-md mx-auto bg-white rounded-lg shadow-lg p-6 lg:p-8 relative">
+                                        <form className="space-y-4 relative" onSubmit={handleUpdateListing}>
+                                            <div>
+                                                <label htmlFor="updatedPrice" className="text-sm xl:text-md font-semibold gap-1 text-gray-700 flex justify-start items-center">
+                                                    <p>Price $</p>
+                                                    <input
+                                                        value={updatedPrice}
+                                                        onChange={handleUpdatePriceInput}
+                                                        className="flex-1 px-1 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                        type="text"
+                                                        id="updatedPrice"
+                                                        name="updatedPrice"
+                                                        required
+                                                    />
+                                                </label>
+                                            </div>
+                                            <div className="flex flex-col justify-center items-center gap-4 w-full">
+                                                <button
+                                                    type="submit"
+                                                    className="w-full max-w-md bg-primary1 hover:bg-indigo-700 text-white font-semibold py-3 px-4 rounded transition duration-300 ease-in-out"
+                                                    disabled={loadingFunction}
+                                                >
+                                                    {loadingFunction ? <LoaderTwo /> : 'Update Listing'}
+                                                </button>
+                                                <div
+                                                    onClick={(() => setShowUpdateForm(false))}
+                                                    className="w-full grid place-content-center max-w-md bg-accent3 hover:bg-indigo-700 text-white font-semibold py-3 px-4 rounded transition duration-300 ease-in-out"
+                                                    disabled={loadingFunction}
+                                                >
+                                                    Cancel
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                ) : (
+                                    <>
+                                        <div
+                                            className='w-full max-w-md cursor-pointer sm:max-w-96 lg:max-w-full text-white font-semibold bg-accent2  p-3 lg:p-4 rounded grid place-content-center'
+                                            onClick={() => setShowUpdateForm(true)}
+                                        >
+                                            Update Listing
+                                        </div>
+                                        <div
+                                            className='w-full max-w-md cursor-pointer sm:max-w-96 lg:max-w-full text-white font-semibold bg-accent3 p-3 lg:p-4 rounded flex justify-center items-center'
+                                            onClick={() => {
+                                                if (deleteListing) {
+                                                handleDeleteListing();
+                                                } else {
+                                                setDeleteListing(true);
+                                                }
+                                            }}
+                                        >
+                                              {loadingDelete ? <LoaderTwo /> : (deleteListing ? 'Are you sure?' : 'Delete Listing')}
+                                        </div>
+
+                                    </>
+                                )}
                             </div>
-                            <div className='flex flex-col gap-1 justify-center items-center p-4 lg:p-6 shadow-lg w-max rounded-lg'>
-                                <p className='text-gray-700 text-md lg:text-lg font-bold'>$ {abtInfo.priceUsd}</p>
-                                <p className='text-gray-700 text-sm xl:text-md font-medium leading-3'>Price</p>
-                            </div>
-                        </div>
+                        </>
                     ) : (
                     abtInfo.isApproved ? (
                         <div className="w-full max-w-md mx-auto bg-white rounded-lg shadow-lg p-6 lg:p-8">
@@ -212,7 +314,7 @@ const ABTDetails = ({ client, market, abt }) => {
                             className="w-full cursor-pointer bg-primary1 hover:bg-indigo-700 text-white font-semibold border-slate-400 border-[1px] p-3 lg:p-4 rounded flex justify-center items-center"
                             disabled={loadingFunction}
                         >
-                        {loadingFunction ? <LoaderTwo /> : 'Approve for Market'}
+                            {loadingFunction ? <LoaderTwo /> : 'Approve for Market'}
                         </div>
                     )
                     )
@@ -233,7 +335,7 @@ const ABTDetails = ({ client, market, abt }) => {
                             className="flex-1 w-full sm:max-w-96 lg:max-w-full bg-green-500 text-white font-semibold py-3 px-4 rounded transition duration-300 ease-in-out"
                             disabled={loadingFunction}
                         >
-                            Buy Now
+                            {loadingFunction ? <LoaderTwo /> : 'Buy Now'}
                         </button>
                     </div>
                 ) : (
