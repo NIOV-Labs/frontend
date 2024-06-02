db0151375e32efbd6f3d5ff2adf2d6d6955e5e21
mar69287 <marcoa.ruiz97@gmail.com>
2024-06-02 10:15:48 -0700

    update home page css

--- src/components/HomeComponents/Card2.jsx
+++ src/components/HomeComponents/Card2.jsx
@@ -3,12 +3,15 @@ import Footer from '../../assets/Footer.svg'
 
 const Card2 = () => {
   return (
-    <div className={`w-full px-5 py-10 lg:py-5 flex justify-center items-center`}>
+    <div className={`w-full px-5 md:px-10 py-10 lg:py-5 flex justify-center items-center`}>
        <div className='border-gray-200 border w-full max-w-6xl rounded p-5 md:p-16 lg:p-20 overflow-hidden flex flex-col justify-center items-start gap-4 relative bg-gradient-to-r from-[#FDB3CB] via-white to-[#C6B5FC]'>
             <h1 className="text-2xl lg:text-3xl xl:text-5xl font-semibold max-w-[26rem] xl:max-w-[38rem] relative z-10">The future of creating and managing Real World Assets (RWAs) in the blockchain industry.</h1>
             <div className="flex justify-start items-center gap-2 relative z-10">
-                <Link to={'/home'} className="rounded-full text-white bg-black py-2 px-3 text-sm">
+                {/* <Link to={'/home'} className="rounded-full text-white bg-black py-2 px-3 text-sm">
                     Learn More
+                </Link> */}
+                <Link to={'/'} className="rounded border-2 border-primary1 text-primary1 py-2 px-3 text-sm md:text-base font-semibold md:py-3 md:px-5 ">
+                    Learn more
                 </Link>
           </div>
           <div className="absolute right-[-12rem] top-[-2] hidden md:flex">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({client, web3Handler, hasWeb3}) => {
     {
       title: 'What are ABTs',
       description: 'Asset Bound Tokens (ABTs) by NIOV Labs redefine digital ownership, merging innovation with flexibility. Powered by our Federation Cloud (FDC). ABTs infuse true dynamic utility aesthetics to a new level of engagement and asset personalization.',
-      button1: 'Contact Sales',
+      button1: 'Learn More',
       button2: 'Learn More',
       image: WhatAbts,
       imageBackground: 'from-[#f2fcfb] via-[#e8fcfa] to-[#f2fcfb]',
@@ -22,7 +22,7 @@ const Home = ({client, web3Handler, hasWeb3}) => {
     {
       title: 'Integration with Federation Cloud (FDC)',
       description: 'Seamlessly integrate with Federation Cloud (FDC) to unlock the full potential of you Asset Bound Tokens (ABTs). FDC`s powerful platform enhances your asset management capabilities, enabling real-time analytics, compliance, and effortless scalability. Transform how you interact with digital assets, making each experience more efficient, secure, and personalized.',
-      button1: 'Contact Sales',
+      button1: '',
       button2: '',
       image: FDC,
       imageBackground: 'from-[#FFECF6] via-[#FFE0F0] to-[#FFECF6]',
@@ -30,8 +30,8 @@ const Home = ({client, web3Handler, hasWeb3}) => {
     },
     {
       title: 'Integration with Chainlink Ecosystem',
-      description: 'Seamlessly integrate with Federation Cloud (FDC) to unlock the full potential of you Asset Bound Tokens (ABTs). FDC`s powerful platform enhances your asset management capabilities, enabling real-time analytics, compliance, and effortless scalability. Transform how you interact with digital assets, making each experience more efficient, secure, and personalized.',
-      button1: '',
+      description: 'Maximize the potential of your digital assets through our integration with Chainlink’s Cross Chain Interoperability Protocol (CCIP) and Data Feeds. This powerful combination enables your Asset Bound Tokens (ABTs) to seamlessly interact across different blockchain networks and leverage real-time, reliable data.',
+      button1: 'Get Started',
       button2: 'Learn More',
       image: Chainlink,
       imageBackground: 'from-white via-white to-[#EAE3FD]',
@@ -41,8 +41,8 @@ const Home = ({client, web3Handler, hasWeb3}) => {
   return (
     <>
       <HomeNavBar client={client} hasWeb3={hasWeb3} web3Handler={web3Handler}/>
-      <div className='mt-[4.5rem] xl:mt-[5rem] w-full mx-auto flex flex-col justify-center items-center lg:gap-10 overflow-hidden'>
-        <Hero />
+      <div className='mt-[4.5rem] md:mt-24 w-full mx-auto flex flex-col justify-center items-center lg:gap-10 overflow-hidden'>
+        <Hero hasWeb3={hasWeb3} web3Handler={web3Handler} />
         {cardInfo.map((card, index) => {
           return (
             <Card1 key={index} title={card.title} description={card.description} button1={card.button1} button2={card.button2} image={card.image} gradient={card.imageBackground} direction={card.tileDirection} />
